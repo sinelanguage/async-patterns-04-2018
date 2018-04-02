c8aceb2dd4ef0cbf97d832b66a5ff89c99c84247
sinelanguage <sinelanguage@gmail.com>
2018-04-02 17:18:00 -0400

    created simple promise

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './style/style.css';
+import simplePromiseExample from './promises';
 // uses style-loader and adds style tag to the top of the index.html because we
 // are using a url import style from "./style/style.css"; uses style-loader and
 // inlines the style
 
-console.log('Hello world');
+simplePromiseExample
+  .then(val => console.log('Value: ', val))
+  .catch(err => console.log('Error: ', err.message));
 
 function foo() {
   const element = document.createElement('h1');
